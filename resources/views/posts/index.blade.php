340ce1844d912dc1df890503c07a2457c34ef179
takada2024 <shotat0722@gmail.com>
2024-05-23 23:32:25 +0900

    クエリビルダを使ってデータを取得しよう

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投稿一覧</title>
+    <title>Posts</title>
 </head>
 <body>
-    <h1>投稿一覧</h1>
+    <h1>Posts</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>
