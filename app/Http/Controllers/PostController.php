0cd21e59a441eb8de7a2672c4b2d10dc882d697b
takada2024 <shotat0722@gmail.com>
2024-05-25 01:29:51 +0900

    Eloquent ORMを使ってデータを取得しよう

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -11,5 +12,14 @@ class PostController extends Controller
     {
         $posts = DB::table('posts')->get();
         return view('posts.index',  compact('posts'));
+    }
+
+    public function show($id)
+    {
+        // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
+        $post = Post::find($id);
+
+        // 変数$productをproducts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/{id}', [PostController::class, 'show']);
